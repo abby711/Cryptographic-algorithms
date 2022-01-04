00d4e87ea6ca1b4bbbfc807d28a3e21a755656a0
Abirami <hindhuabisundaram@gmail.com>
2022-01-04 23:19:58 +0530

    Update playfair.cpp

--- abbyCodes/playfair.cpp
+++ abbyCodes/playfair.cpp
@@ -1,128 +1,92 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
 #include<string>
-#include<vector>
 using namespace std;
-char keyMatrix[5][5];
-int count[26]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-
+char keyM[5][5];
+int coun[26]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 void setMatrix(){
-    for(int i=0;i<5;i++){
-        for(int j=0;j<5;j++){
-            keyMatrix[i][j]=' ';
-        }
-    }
-}
-
-
-
-vector<int> findIndex(char ch){
-    vector<int> index;
     for(int i=0;i<5;i++)
-    {
-        for(int j=0;j<5;j++)
-        {
-            if (keyMatrix[i][j]==ch)
-            {
-                index.push_back(i);
-                index.push_back(j);
-                return index;
-            }
-             
-        }
-    }
+    for(int j=0;j<5;j++) keyM[i][j]='\0';
 }
 int main() {
+    // Write C++ code here
+    setMatrix();
     string key,pt;
     cin>>key;
     cin>>pt;
-    int temp,temp2;
-    setMatrix();
-    int index=0;
-
-    for(int i=0;i<5;i++)
+    int index=0,x;
+    for(int i=0;i<5 && index<key.length();i++)
+    for(int j=0;j<5 && index<key.length();j++)
     {
-        for(int j=0;j<5;j++)
+        
+        x=key[index];
+        x=x-97;
+        if(coun[x]==0)
         {
-            if(key[index]=='i' || key[index]=='j')
+            if(x==8 || x==9)
             {
-                temp=key[index]-97;
-                if(key[index]=='i' )
-                   temp2=temp+1;  
-                else
-                   temp2=temp-1;
-                
-                if(count[temp]==0 && count[temp2]==0) 
-                    { count[temp]=1; 
-                      count[temp2]=1;
-                      keyMatrix[i][j]='i';
-                      index++;
-                    }
+                coun[8]=1;
+                coun[9]=1;
+                keyM[i][j]='i';
+                index++;
+
             }
             else{
-                 temp=key[index]-97;
-                if(count[temp]==0) 
-                    { count[temp]=1; 
-                      
-                      keyMatrix[i][j]=key[index];
-                      index++;
-                    }
+            keyM[i][j]=key[index];
+            coun[x]=1;
+            index++;
+            }
+            
+        }
+        else if(coun[x]==1)
+           index++;
+        
+        
+    }
+    index=0;
+    int flag=0;
+    
+    for(int i=0;i<5 && index<26;i++)
+    for(int j=0;j<5 && index<26;j++)
+    {   
+        flag=0;
+        if(keyM[i][j]=='\0')
+        {   
+            for(int j=0;j<26 && flag==0;j++)
+        {
+            index=j;
+            if(coun[index]==0)
+            {
+                if (index==8 || index==9){
+                    coun[8]=1;
+                    coun[9]=1;
+                    keyM[i][j]='i';
+                    index++;
+                    flag=1;
+                    
+                }
+                else{
+                    coun[index]=1;
+                    int x=index+97;
+                    char ch=x;
+                    keyM[i][j]=ch;
+                    index++;
+                    flag=1;
+                    
+                }
             }
             
         }
     }
-    char ch;
-    int flag=0;
-    for(int i=0;i<5;i++)
-    {
-        for(int j=0;j<5;j++)
-        {
-            
-            if(keyMatrix[i][j]==' ')
-            {   flag=0;
-                for(int k=0;k<26 && flag!=1;k++)
-                 {
-                    
-                     if (count[k]==0)
-                     {
-                       if(k==13 || k==14)
-                     {
-                        if (count[13]==0 && count[14]==0)
-                        {
-                         ch=char(k+97);
-                         count[13]=0;
-                         count[14]=0;
-                         keyMatrix[i][j]='i';
-                         flag=1;
-                       
-                        }
-                    } 
-                     else
-                     {
-                       int l=k+97;
-                       ch=l;
-                       count[k]=0;
-                       keyMatrix[i][j]=ch;
-                       flag=1;
-                     }
-                }
-                     else
-                      continue;
-                       
-                 }
-                }
-            }
-        }
+    }
     
     
     for(int i=0;i<5;i++){
         cout<<endl;
-        for(int j=0;j<5;j++)
-        cout<<keyMatrix[i][j]<<" ";
     
+    for(int j=0;j<5;j++)
+    cout<<keyM[i][j]<<" ";
+    }
     
-    
-
-}
-return 0;
+    return 0;
 }
