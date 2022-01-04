b3ed44afd3a7a08a905b42546c0f622e1a9d85f9
Abirami <hindhuabisundaram@gmail.com>
2022-01-04 21:54:24 +0530

    Update digitalSignatureStandard.java

--- digitalSignatureStandard.java
+++ digitalSignatureStandard.java
@@ -2,8 +2,10 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.Signature;
 import sun.misc.BASE64Encoder;
-public class DigSign {
-public static void main(String[] args) throws Exception {
+public class DigSign 
+{
+public static void main(String[] args) throws Exception 
+{
 KeyPairGeneratorkpg = KeyPairGenerator.getInstance("RSA");
 kpg.initialize(1024);
 KeyPairkeyPair = kpg.genKeyPair();
@@ -12,8 +14,9 @@ Signature sig = Signature.getInstance("MD5WithRSA");
 sig.initSign(keyPair.getPrivate());
 sig.update(data);
 byte[] signatureBytes = sig.sign();
-System.out.println("Signature: \n" + new BASE64Encoder().encode(signatureBytes));
+System.out.println("Signature: "+signature);
 sig.initVerify(keyPair.getPublic());
 sig.update(data);
 System.out.println(sig.verify(signatureBytes));
-} }
+} 
+}
